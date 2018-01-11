3f8c2746cd1c37935ac3d026cadc315b6a7360d1
鳥居隆弘 <torii@mac133.local>
2018-01-11 16:17:45 +0900

    first commit

--- PullRefleshTest/ViewController.swift
+++ PullRefleshTest/ViewController.swift
@@ -9,17 +9,198 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var data: [ String ] = ["1","2","3"]
+    
+    var refreshControl: UIRefreshControl!
+    
+    var customView: UIView!
+    
+    var labelsArray: Array<UILabel> = []
+    
+    var isAnimating = false
+    
+    var timer: Timer?
+    
+    var currentColorIndex = 0
+    
+    var currentLabelIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        //デリゲート
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        //リフレッシュコントロールセット
+        refreshControl = UIRefreshControl()
+        refreshControl.backgroundColor = .clear
+        refreshControl.tintColor = .clear
+        tableView.addSubview(refreshControl) //?? addsubview??
 
+        
+        //refreshControl.addTarget(self, action: #selector( ViewController.reflesh(_:)), for: .valueChanged)
+        //tableView.refreshControl = refreshControl
+        
+        loadCustomRefreshContents()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+extension ViewController {
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+    
+    @objc func reflesh(_: Any) {
+        tableView.refreshControl?.endRefreshing()
+    }
+
+    func doSomething() {
+        timer = Timer.scheduledTimer(timeInterval: 4.0, target: self, selector: #selector( ViewController.endOfWork(_:)), userInfo: nil, repeats: true)
+    }
+
+    @objc func endOfWork(_: Any) {
+        refreshControl.endRefreshing()
+        
+        timer?.invalidate()
+        timer = nil
+    }
+    
+    func loadCustomRefreshContents() {
+        //let refreshContents = Bundle.main.loadNibNamed("RefreshContents", owner: self, options: nil) //PullToRefreshView
+        let refreshContents = Bundle.main.loadNibNamed("PullToRefreshView", owner: self, options: nil) //PullToRefreshView
+        customView = refreshContents![0] as! UIView
+        customView.frame = refreshControl.bounds
+        
+        for i in 0..<customView.subviews.count {
+            labelsArray.append(customView.viewWithTag(i + 1) as! UILabel)
+        }
+        
+        refreshControl.addSubview(customView)
+    }
+    
+    func animateRefreshStep1() {
+        isAnimating = true
+        
+        UIView.animate(withDuration: 0.1, delay: 0.0, options: UIViewAnimationOptions.curveLinear, animations: { () -> Void in
+            self.labelsArray[self.currentLabelIndex].transform = CGAffineTransform(rotationAngle: CGFloat(M_PI_4))
+            self.labelsArray[self.currentLabelIndex].textColor = self.getNextColor()
+            
+        }, completion: { (finished) -> Void in
+            
+            UIView.animate(withDuration: 0.05, delay: 0.0, options: UIViewAnimationOptions.curveLinear, animations: { () -> Void in
+                self.labelsArray[self.currentLabelIndex].transform = CGAffineTransform.identity
+                self.labelsArray[self.currentLabelIndex].textColor = UIColor.black
+                
+            }, completion: { (finished) -> Void in
+                self.currentLabelIndex += 1
+                
+                if self.currentLabelIndex < self.labelsArray.count {
+                    self.animateRefreshStep1()
+                }
+                else {
+                    self.animateRefreshStep2()
+                }
+            })
+        })
+    }
+    
+    func animateRefreshStep2() {
+        UIView.animate(withDuration: 0.35, delay: 0.0, options: UIViewAnimationOptions.curveLinear, animations: { () -> Void in
+            self.labelsArray[0].transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+            self.labelsArray[1].transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+            self.labelsArray[2].transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+            self.labelsArray[3].transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+            self.labelsArray[4].transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+            self.labelsArray[5].transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+            self.labelsArray[6].transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+            
+        }, completion: { (finished) -> Void in
+            UIView.animate(withDuration: 0.25, delay: 0.0, options: UIViewAnimationOptions.curveLinear, animations: { () -> Void in
+                self.labelsArray[0].transform = CGAffineTransform.identity
+                self.labelsArray[1].transform = CGAffineTransform.identity
+                self.labelsArray[2].transform = CGAffineTransform.identity
+                self.labelsArray[3].transform = CGAffineTransform.identity
+                self.labelsArray[4].transform = CGAffineTransform.identity
+                self.labelsArray[5].transform = CGAffineTransform.identity
+                self.labelsArray[6].transform = CGAffineTransform.identity
+                
+            }, completion: { (finished) -> Void in
+                if self.refreshControl.isRefreshing {
+                    self.currentLabelIndex = 0
+                    self.animateRefreshStep1()
+                }
+                else {
+                    self.isAnimating = false
+                    self.currentLabelIndex = 0
+                    
+                    for i in 0..<self.labelsArray.count {
+                        self.labelsArray[i].textColor = UIColor.black
+                        self.labelsArray[i].transform = CGAffineTransform.identity
+                    }
+                }
+            })
+        })
+    }
+    
+    func getNextColor() -> UIColor {
+        var colorsArray: Array<UIColor> = [ .magenta, .brown, .yellow, .red, .green, .blue, .orange]
+        
+        if currentColorIndex == colorsArray.count {
+            currentColorIndex = 0
+        }
+        
+        let returnColor = colorsArray[currentColorIndex]
+        currentColorIndex += 1
+        
+        return returnColor
+    }
+    
+}
+
+extension ViewController: UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        cell.backgroundColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return
+            data.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "ID", sender: nil)
+        
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        print(#function)
+        if refreshControl.isRefreshing {
+            if !isAnimating {
+                doSomething()
+                animateRefreshStep1()
+            }
+        }
+    }
+}
